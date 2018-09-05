019793b31c8005fa21f4024a52c7c5a313d896a7
yanhua0518 <42855794+yanhua0518@users.noreply.github.com>
2018-09-05 17:58:42 +0900

    Add files via upload

--- SiglusEngine/ScenePacker.py
+++ SiglusEngine/ScenePacker.py
@@ -3,10 +3,8 @@
 
 import sys
 import os
-from io import BytesIO
-import glob
+from ctypes import *
 import struct
-import unicodedata
 
 class Header:
     length=0
@@ -52,101 +50,74 @@ class Header:
         H.SourceHeaderLength=struct.unpack('I',f.read(4))[0]
 
 def Decrypt1(string):
-    #Change this key to your game's key.
+    # Change this key to your game's key.
     key=[0x2E, 0x4B, 0xDD, 0x2A, 0x7B, 0xB0, 0x0A, 0xBA,
          0xF8, 0x1A, 0xF9, 0x61, 0xB0, 0x18, 0x98, 0x5C]
-    newString=b''
-    '''
-    for n in range(0,len(string)):
-        newString+=bytes([string[n]^key[n&15]])
-    '''
-    n=0
-    for char in string:
-        newString+=bytes([char^key[n]])
-        n+=1
-        n&=15
-    
-    return newString
+    size=len(string)
+    keyBuf=c_char_p(struct.pack('16B',*key))
+    dll.decrypt1(string,size,keyBuf)
+    return string
 
 def Decrypt2(string):
-    key=[0x70,0xF8,0xA6,0xB0,0xA1,0xA5,0x28,0x4F,
-         0xB5,0x2F,0x48,0xFA,0xE1,0xE9,0x4B,0xDE,
-         0xB7,0x4F,0x62,0x95,0x8B,0xE0,0x03,0x80,
-         0xE7,0xCF,0x0F,0x6B,0x92,0x01,0xEB,0xF8,
-         0xA2,0x88,0xCE,0x63,0x04,0x38,0xD2,0x6D,
-         0x8C,0xD2,0x88,0x76,0xA7,0x92,0x71,0x8F,
-         0x4E,0xB6,0x8D,0x01,0x79,0x88,0x83,0x0A,
-         0xF9,0xE9,0x2C,0xDB,0x67,0xDB,0x91,0x14,
-         0xD5,0x9A,0x4E,0x79,0x17,0x23,0x08,0x96,
-         0x0E,0x1D,0x15,0xF9,0xA5,0xA0,0x6F,0x58,
-         0x17,0xC8,0xA9,0x46,0xDA,0x22,0xFF,0xFD,
-         0x87,0x12,0x42,0xFB,0xA9,0xB8,0x67,0x6C,
-         0x91,0x67,0x64,0xF9,0xD1,0x1E,0xE4,0x50,
-         0x64,0x6F,0xF2,0x0B,0xDE,0x40,0xE7,0x47,
-         0xF1,0x03,0xCC,0x2A,0xAD,0x7F,0x34,0x21,
-         0xA0,0x64,0x26,0x98,0x6C,0xED,0x69,0xF4,
-         0xB5,0x23,0x08,0x6E,0x7D,0x92,0xF6,0xEB,
-         0x93,0xF0,0x7A,0x89,0x5E,0xF9,0xF8,0x7A,
-         0xAF,0xE8,0xA9,0x48,0xC2,0xAC,0x11,0x6B,
-         0x2B,0x33,0xA7,0x40,0x0D,0xDC,0x7D,0xA7,
-         0x5B,0xCF,0xC8,0x31,0xD1,0x77,0x52,0x8D,
-         0x82,0xAC,0x41,0xB8,0x73,0xA5,0x4F,0x26,
-         0x7C,0x0F,0x39,0xDA,0x5B,0x37,0x4A,0xDE,
-         0xA4,0x49,0x0B,0x7C,0x17,0xA3,0x43,0xAE,
-         0x77,0x06,0x64,0x73,0xC0,0x43,0xA3,0x18,
-         0x5A,0x0F,0x9F,0x02,0x4C,0x7E,0x8B,0x01,
-         0x9F,0x2D,0xAE,0x72,0x54,0x13,0xFF,0x96,
-         0xAE,0x0B,0x34,0x58,0xCF,0xE3,0x00,0x78,
-         0xBE,0xE3,0xF5,0x61,0xE4,0x87,0x7C,0xFC,
-         0x80,0xAF,0xC4,0x8D,0x46,0x3A,0x5D,0xD0,
-         0x36,0xBC,0xE5,0x60,0x77,0x68,0x08,0x4F,
-         0xBB,0xAB,0xE2,0x78,0x07,0xE8,0x73,0xBF]
-    newString=b''
-    '''
-    for n in range(0,len(string)):
-        newString+=bytes([string[n]^key[n&255]])
-    '''
-    n=0
-    for char in string:
-        newString+=bytes([char^key[n]])
-        n+=1
-        n&=255
-    
-    return newString
+    size=len(string)
+    dll.decrypt2(string,size)
+    return string
+
+def FakeCompress(string):
+    ssSize=len(string)
+    dataSize=ssSize+int(ssSize/8)
+    if not ssSize%8==0:
+        dataSize+=1
+    newString=bytes(dataSize)
+    dll.fakeCompress(string,newString,ssSize)
+    return struct.pack('2I',dataSize,ssSize)+newString
 
 def Compress(string):
     length=len(string)
-    newString=b''
-    for n in range(0,int(length/8)):
-        newString+=b'\xff'+string[n*8:n*8+8]
-    if not length%8==0:
-        last=string[(n+1)*8:]
-        newString+=bytes([255>>(8-length%8)])+last
+    size=c_int(0)
+    p=Xmoe.CompressData(string,length,pointer(size),17)
+    newString=string_at(p,size)
     return newString
+    
 
-
-if len(sys.argv) < 3:
-    print ("Usage: "+sys.argv[0]+" <Scene.pck> <Scene\> [Scene.pck2]")
+argv=sys.argv
+if argv.count('-c')>0:
+    doComp=True
+    argv.remove('-c')
+else:
+    doComp=False
+    
+if len(argv)<3:
+    print ("Usage: "+argv[0][argv[0].rfind("\\")+1:]+" <Scene.pck> <Scene\> [Scene.pck2] [-c]")
     quit()
 
-
-if len(sys.argv) < 4:
+if len(argv)<4:
     outFN="Scene.pck2"
 else:
-    outFN=sys.argv[3]
-inF=sys.argv[0][:sys.argv[0].rfind("\\")+1]+sys.argv[2]+"\\"
+    outFN=argv[3]
+
+inF=argv[0][:argv[0].rfind("\\")+1]+argv[2]+"\\"
 try:
-    f=open(sys.argv[1],'rb')
+    f=open(argv[1],'rb')
     f.read()
     f.close()
 except:
     quit()
+    
+try:
+    dll=CDLL('Decryption.dll')
+except:
+    print("Can't open Decryption.dll")
+    quit()
+if doComp:
+    try:
+        Xmoe=CDLL('Compression.dll')
+    except:
+        print("Can't open Compression.dll")
+        quit()
 
-
-
-
-size=os.path.getsize(sys.argv[1])
-scene=open(sys.argv[1],'rb')
+size=os.path.getsize(argv[1])
+scene=open(argv[1],'rb')
 header=Header(scene)
 
 scene.seek(header.SceneNameIndexOffset)
@@ -176,7 +147,7 @@ for n in range(0,header.SceneDataCount):
     
 
 for n in range(0,header.SceneDataCount):
-    fileName='%.3d.'%n+SceneNameString[n].decode("UTF-16")+'.ss'
+    fileName=SceneNameString[n].decode("UTF-16")+'.ss'
     try:
         f=open(inF+fileName,'rb')
         f.read()
@@ -185,21 +156,18 @@ for n in range(0,header.SceneDataCount):
         continue
     print(fileName)
     ssFile=open(inF+fileName,'rb')
-    ssSize=os.path.getsize(inF+fileName)
     data=ssFile.read()
-    #No compression yet, this is fake.
-    dataSize=ssSize+int(ssSize/8)+8
-    if not ssSize%8==0:
-        dataSize+=1
-    compData=struct.pack('2I',dataSize,ssSize)
-    compData+=Compress(data)
+    if doComp:
+        compData=Compress(data)
+    else:
+        compData=FakeCompress(data)
     '''
     compFile=open(inF+fileName+'.compressed','wb')
     compFile.write(compData)
     compFile.close()
     '''
     SceneData[n]=Decrypt1(Decrypt2(compData))
-    SceneDataLength[n]=dataSize
+    SceneDataLength[n]=len(compData)
     ssFile.close()
     
 
