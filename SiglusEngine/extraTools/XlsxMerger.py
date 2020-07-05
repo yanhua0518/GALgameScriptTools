8b093197413c2a4bec71512b5388e3ccb1f10b90
yanhua0518 <42855794+yanhua0518@users.noreply.github.com>
2020-07-05 18:52:02 +0800

    xlsx tools

---
+++ SiglusEngine/extraTools/XlsxMerger.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# For Windows OS Only....
+
+import sys
+import os
+import glob
+import openpyxl
+
+def main(argv):
+    
+    if len(argv)<2 or argv[1]=='':
+        print ("Usage: "+argv[0][argv[0].rfind("\\")+1:]+" <Excel folder\>")
+        return False
+
+    inF=argv[1]+"\\"
+    outXLS=argv[0][:argv[0].rfind("\\")+1]+argv[1]+".xlsx"
+    mixBook=openpyxl.Workbook()
+    mixBook.remove(mixBook.active)
+    for inFN in glob.glob(inF+"*.xlsx"):
+        print(inFN)
+        workBook=openpyxl.load_workbook(inFN)
+        for sheet in workBook:
+            workSheet=mixBook.create_sheet(sheet.title)
+            for i,row in enumerate(sheet.iter_rows()):
+                for j,cell in enumerate(row):
+                    workSheet.cell(row=i+1,column=j+1,value=cell.value)
+    moxBook.save(outXLS)
+    return True
+
+if __name__=="__main__":
+    main(sys.argv)
---
+++ SiglusEngine/extraTools/txt2xlsx.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# For Windows OS Only....
+
+import sys
+import os
+import glob
+import openpyxl
+
+
+def main(argv):
+    txtF='text\\'
+    xlsF='Excel_file\\'
+
+    if not os.path.exists(xlsF):
+        os.makedirs(xlsF)
+    for inFN in glob.glob(txtF+"*.txt"):
+        print(inFN)
+        outFN=xlsF+inFN[inFN.rfind('\\'):].replace(".txt",".xlsx")
+        try:
+            txtFile=open(inFN,'r',1,"UTF-8")
+            if txtFile.read(1)!="\ufeff":
+                txtFile.seek(0)
+        except:
+            continue
+        workBook=openpyxl.Workbook()
+        workSheet=workBook.active
+        workSheet.title=inFN[inFN.rfind('\\')+1:].replace(".txt",".ss").replace(".ss.ss",".ss")
+        workSheet.append(["Index","Text","Translation"])
+        workSheet.column_dimensions['A'].width=8
+        workSheet.column_dimensions['B'].width=64
+        workSheet.column_dimensions['C'].width=64
+        index=0
+        text=""
+        trans=""
+        for line in txtFile.readlines():
+            if line[0]=="○":
+                index=int(line[1:line.find("○",1)])
+                text=line[line.find("○",1)+1:].replace("\n","")
+            elif line[0]=="●":
+                trans=line[line.find("●",1)+1:].replace("\n","")
+                workSheet.append([index,text,trans])
+        txtFile.close()
+        workBook.save(outFN)
+    return True
+
+if __name__=="__main__":
+    main(sys.argv)
