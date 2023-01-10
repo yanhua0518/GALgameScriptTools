912ed711f2d567d8b977c1f5515a0faec8c4ac60
yanhua0518 <42855794+yanhua0518@users.noreply.github.com>
2023-01-10 21:59:05 +0800

    Add files via upload

--- SiglusEngine/extraTools/XlsxMerger.py
+++ SiglusEngine/extraTools/XlsxMerger.py
@@ -4,12 +4,17 @@
 import sys
 import os
 import glob
+from copy import copy,deepcopy
 import openpyxl
 
 def main(argv):
-    
+    if argv.count("-t"):
+        addFN=True
+        argv.remove("-t")
+    else:
+        addFN=False
     if len(argv)<2 or argv[1]=='':
-        print ("Usage: "+argv[0][argv[0].rfind("/")+1:]+" <Excel folder\>")
+        print ("Usage: "+argv[0][argv[0].rfind("/")+1:]+" <Excel folder\> [-t]")
         return False
 
     inF=argv[1]+"/"
@@ -20,10 +25,29 @@ def main(argv):
         print(inFN)
         workBook=openpyxl.load_workbook(inFN)
         for sheet in workBook:
-            workSheet=mixBook.create_sheet(sheet.title)
-            for i,row in enumerate(sheet.iter_rows()):
-                for j,cell in enumerate(row):
-                    workSheet.cell(row=i+1,column=j+1,value=cell.value)
+            title=inFN[inFN.rfind("\\")+1:].replace("[","(").replace("]",")").replace(".xlsx","")
+            #print(title)
+            if sheet.max_column==1 and sheet.max_row==1:
+                continue
+            if addFN:
+                workSheet=mixBook.create_sheet(title+" "+sheet.title)
+            else:
+                workSheet=mixBook.create_sheet(sheet.title)
+            print(workSheet.title)
+            for row in sheet.rows:
+                for cell in row:
+                    workSheet[cell.coordinate].value=copy(cell.value)
+                    workSheet[cell.coordinate].font=copy(cell.font)
+                    workSheet[cell.coordinate].border=copy(cell.border)
+                    workSheet[cell.coordinate].fill=copy(cell.fill)
+                    workSheet[cell.coordinate].number_format=copy(cell.number_format)
+                    workSheet[cell.coordinate].alignment=copy(cell.alignment)
+            for i in sheet.column_dimensions:
+                workSheet.column_dimensions[i].width=sheet.column_dimensions[i].width
+            for i in sheet.row_dimensions:
+                workSheet.row_dimensions[i].height=sheet.row_dimensions[i].height
+                
+
     mixBook.save(outXLS)
     return True
 
