9804934724fc83e311a13bd3105e79c9ae2ccb27
yanhua0518 <42855794+yanhua0518@users.noreply.github.com>
2023-03-25 17:47:21 +0800

    Add special mode

--- SiglusEngine/ssDumper.py
+++ SiglusEngine/ssDumper.py
@@ -40,6 +40,9 @@ def check(scr,checkHalf):
                 return True
         return False
 
+def softIndex(l,c,e):
+    return l*1000000+c*1000+e
+
 def main(argv):
     if argv.count('-a')>0:
         noDump=True
@@ -72,9 +75,19 @@ def main(argv):
         argv.remove('-c')
     else:
         countWords=False
+    if argv.count('-o')>0:
+        offMode=True
+        argv.remove('-o')
+        '''
+        LINE=1000000
+        CHAR=1000
+        END=1
+        '''
+    else:
+        offMode=False
     
     if len(argv)<2 or argv[1]=='':
-        print ("Usage: "+argv[0][argv[0].rfind("\\")+1:]+" <Scene\> [Text\] [-d] [-a/-w] [-x [-s [-c]]]")
+        print ("Usage: "+argv[0][argv[0].rfind("\\")+1:]+" <Scene\> [Text\] [-o] [-d] [-a/-w] [-x [-s [-c]]]")
         return False
     countError=0
     inF=argv[1]+"\\"
@@ -106,7 +119,7 @@ def main(argv):
                 workSheet.title=sheet
             else:
                 workSheet=workBook.create_sheet(sheet)
-            workSheet.column_dimensions['A'].width=8
+            workSheet.column_dimensions['A'].width=6
             workSheet.column_dimensions['B'].width=64
             workSheet.column_dimensions['C'].width=64
             fillColor=openpyxl.styles.PatternFill(fill_type="solid", fgColor="F2F2F2")
@@ -124,44 +137,117 @@ def main(argv):
                 print("Output file error!")
                 countError+=1
                 continue
-        try:
-            size=os.path.getsize(inFN)
-            file=open(inFN,'rb')
-            header=Header(file)
-        except:
-            print("Input file error!")
-            countError+=1
-            continue
-        file.seek(header.index)
-        offset=[]
-        length=[]
-        for n in range(0,header.count):
-            offset.append(struct.unpack('I',file.read(4))[0])
-            length.append(struct.unpack('I',file.read(4))[0])
-        
-        for x in range(0,header.count):
-            if length[x]==0:
-                continue
-            file.seek(header.offset+offset[x]*2,0)
-            string=file.read(length[x]*2)
-            text=Decrypt(string,length[x],x).decode("UTF-16")
-            if not check(text,fullDump) and not noDump:
-                continue
-            if xlsxMode:
-                countLen+=len(text)
-                if copyLine:
-                    workSheet.append([x,text,text])
-                else:
-                    workSheet.append([x,text])
-                workSheet.cell(workSheet.max_row,3).fill=fillColor
-                workSheet.cell(workSheet.max_row,3).border=border
+        indexs=[]
+        texts=[]
+        if offMode:
+            try:
+                file=open(inFN,'r',1,"UTF-8")
+                lines=file.readlines()
+                file.close()
+            except UnicodeDecodeError:
+                try:
+                    file=open(inFN,'r',1,"SHIFT-JIS")
+                    lines=file.readlines()
+                    file.close()
+                except:
+                    print("Input file error!")
+                    countError+=1
+            except:
+                print("Input file error!")
+                countError+=1
             else:
-                if copyLine:
-                    outLine="○"+'%.6d'%x+"○"+text+"\n●"+'%.6d'%x+"●"+text+"\n\n"
+                isNote=False
+                for i,line in enumerate(lines):
+                    isText=0
+                    isAt=False
+                    start=0
+                    for n,char in enumerate(line):
+                        if char=='/':
+                            if isNote:
+                                if line[n-1:n]=='*':
+                                    isNote=False
+                            else:
+                                if line[n+1:n+2]=='*':
+                                    isNote=True
+                                elif line[n+1:n+2]=='/':
+                                    break
+                        if isNote or (isText==2 and char!='"'):
+                            continue
+                        if char=='#' or char==';':
+                            break
+                        if char=='\t' or char=='\n':
+                            isAt=False
+                            continue
+                        if char=='@' and line[n+1:n+5]!='ruby':
+                            isAt=True
+                        if isAt:
+                            if char==' ' or char==',' or char=='(' or char==')' or char=='【' or char=='「':
+                                isAt=False
+                        if char=='"':
+                            if isText==2:
+                                isText=0
+                                indexs.append(softIndex(i,start,n-1))
+                                texts.append(line[start:n])
+                            else:
+                                if isText==1:
+                                    indexs.append(softIndex(i,start,n-1))
+                                    texts.append(line[start:n])
+                                isText=2
+                                start=n+1
+                        elif unicodedata.east_asian_width(char)!='Na':
+                            if not isText and not isAt:
+                                isText=1
+                                start=n
+                        elif isText==1:
+                            isText=0
+                            indexs.append(softIndex(i,start,n-1))
+                            texts.append(line[start:n])
+                    if isText:
+                        isText=0
+                        indexs.append(softIndex(i,start,n))
+                        texts.append(line[start:-1])   
+        else:
+            try:
+                file=open(inFN,'rb')
+                header=Header(file)
+                file.seek(header.index)
+                offset=[]
+                length=[]
+                for n in range(0,header.count):
+                    offset.append(struct.unpack('I',file.read(4))[0])
+                    length.append(struct.unpack('I',file.read(4))[0])
+                indexs=[]
+                texts=[]
+                for x in range(0,header.count):
+                    if length[x]==0:
+                        continue
+                    file.seek(header.offset+offset[x]*2,0)
+                    string=file.read(length[x]*2)
+                    text=Decrypt(string,length[x],x).decode("UTF-16")
+                    indexs.append(x)
+                    texts.append(text)
+                file.close()
+            except:
+                print("Input file error!")
+                countError+=1
+        if indexs and texts:
+            for index,text in zip(indexs,texts):
+                if not check(text,fullDump) and not noDump:
+                    continue
+                if xlsxMode:
+                    countLen+=len(text)
+                    if copyLine:
+                        workSheet.append([index,text,text])
+                    else:
+                        workSheet.append([index,text])
+                    workSheet.cell(workSheet.max_row,3).fill=fillColor
+                    workSheet.cell(workSheet.max_row,3).border=border
                 else:
-                    outLine="○"+'%.6d'%x+"○"+text+"\n●"+'%.6d'%x+"●\n\n"
-                output.write(outLine)
-        file.close()
+                    if copyLine:
+                        outLine="○"+'%.10d'%index+"○"+text+"\n●"+'%.10d'%index+"●"+text+"\n\n"
+                    else:
+                        outLine="○"+'%.10d'%index+"○"+text+"\n●"+'%.10d'%index+"●\n\n"
+                    output.write(outLine)
         if xlsxMode:
             textLine=workSheet.max_row
             if textLine==1 and singleXlsx:
